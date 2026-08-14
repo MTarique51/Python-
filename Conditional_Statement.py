b067e968b65b9337adf3b2418d4ea45c78a19d50
MTarique51 <mohdtarique7223@gmail.com>
2026-08-14 09:49:40 +0530

    Conditional Statement

--- Conditional_Statement.py
+++ Conditional_Statement.py
@@ -8,3 +8,16 @@ elif(light == "green"):
     print("Go")
 else:
     print("Light is nor working")
+
+
+# Conditional Statements(Single Line if / Ternary Operator)
+# <var> = <val1> if <condition> else <val2>
+food = input("food: ")
+eat = "Yes" if food == "cake" else "no"
+print("Able to eat ? ", eat)
+
+            # OR #
+
+# <stt1> if <condition> else <stt2>
+food = input("food: ")
+print("sweet") if food == "cake" or food =="Jalebi" else print("No sweet")
