398f25e897de40829267673642395680184514c3
MTarique51 <mohdtarique7223@gmail.com>
2026-08-15 11:59:26 +0530

    Taking input

---
+++ taking_input.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+print("Welcome ", name)
+
+val = input("enter some value: ")
+print(type(val) , val)
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+marks = input("Enter your marks: ")
+
+print("Welcome" , name)
+print("age =", age)
+print("marks= ", marks)
