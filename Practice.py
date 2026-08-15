d4482dc51e83c146157e359f2918c16c1f687ebd
MTarique51 <mohdtarique7223@gmail.com>
2026-08-15 12:26:04 +0530

    Practice

---
+++ Practice.py
@@ -0,0 +1,23 @@
+# write a program to input 2 numbers and print their sum
+
+num1 = int(input("enter num1: "))
+num2 = int(input("enter num2: "))
+sum = num1 + num2
+print(sum)
+
+# Calculate the area of Square
+side = float(input("enter side: "))
+area = side * side 
+print(area)
+
+#taking two floating number and average sum of floating point number 
+n1 = float(input("enter 1st floating number : "))
+n2 = float(input("enter 2nd floating number : "))
+summ = n1 + n2
+print(summ/2)
+
+# WAP to ..........
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+
+print(a >= b)
