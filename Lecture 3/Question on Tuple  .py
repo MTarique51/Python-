351b54c9796a3d297af09353b3c09f39b12ed368
MTarique51 <mohdtarique7223@gmail.com>
2026-08-26 09:29:15 +0530

    Some Questions on Tuple

---
+++ Lecture 3/Question on Tuple  .py
@@ -0,0 +1,26 @@
+# WAP to askk the user to enter names of their 3 favorite movies and store them in a list
+
+# Movies = []
+
+# m1 = input("enter 1st movie: ")
+# Movies.append(m1)
+# m2 = input("enter 2nd movie: ")
+# Movies.append(m2)
+# m3 = input("enter 3rd movie: ")
+# Movies.append(m3)
+ 
+# print(Movies)
+
+
+#  P A L I N D R O M E
+
+list1 = [1,2,2,1]
+list2 = [1,2,2,1,5]
+
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
